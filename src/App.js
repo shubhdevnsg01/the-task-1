51ff32c58f02aea008b2658b4cb1ac79b643fc95
shubhdevnsg01 <petrifiedmaniac@gmail.com>
2023-12-31 10:28:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+ 
+    const apiURL = "https://s3.amazonaws.com/open-to-cors/assignment.json";
+
+    axios.get(apiURL)
+      .then(response => {
+       
+        if (response.status !== 200) {
+          throw new Error(`Failed to fetch data. Status code: ${response.status}`);
+        }
+        return response.data;
+      })
+      .then(data => {
+      
+        const productsArray = Object.values(data.products);
+
+     
+        const sortedProducts = productsArray.sort((a, b) => b.popularity - a.popularity);
+
+      
+        setProducts(sortedProducts);
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Subcategory</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product.title}>
+              <td>{product.subcategory}</td>
+              <td>{product.title}</td>
+             
+              <td>{product.price}</td>
+              <td>{product.popularity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default ProductList;
